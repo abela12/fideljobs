27593c1a5c85f962a76d629b786f8b8599c29ab7
abela12 <abelberhanu6@gmail.com>
2021-07-09 16:27:28 -0600

    header logo updated

--- admin/include/header.php
+++ admin/include/header.php
@@ -1,14 +1,16 @@
 <head>
     <!-- <base href="../../../"> -->
     <meta charset="utf-8">
-    <meta name="author" content="Softnio">
+    <meta name="author" content="BlackCoders">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords"
+        content="Jobs, vacancies, Jobs in Ethiopia, Job search, recruitment in ethiopia, NGO jobs, heineken jobs, Finance Jobs, Marketing Jobs, Sales Jobs, Banking Jobs, Jobs in Addis Ababa, Jobs in Oromia, Jobs Ethiopia, IT Jobs, Management Jobs, Health care Jobs, Administrator Jobs, Accountant Jobs, Ethio jobs,jobs from Employment agencies" />
     <meta name="description"
-        content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+        content="Find latest Jobs in Ethiopia, vacancies in Ethiopia on ፊደልJobs. Apply now for Ethiopian NGO jobs, accounting and finance jobs and IT jobs. Recruitment in Ethiopia. jobs from Employment agencies." />
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="images/icon.png">
     <!-- Page Title  -->
-    <title>Login | DashLite Admin Template</title>
+    <title>Login | ፊደልJObs</title>
     <!-- StyleSheets  -->
     <link rel="stylesheet" href="assets/css/dashlite.css?ver=2.4.0">
     <link id="skin-default" rel="stylesheet" href="assets/css/theme.css?ver=2.4.0">
