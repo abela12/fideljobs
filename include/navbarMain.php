9452751ce7e03b5d498cb4943a6e5adcf52c9fdb
abela12 <abelberhanu6@gmail.com>
2021-07-11 17:24:59 -0600

    update navbar link

--- include/navbarMain.php
+++ include/navbarMain.php
@@ -62,45 +62,10 @@
                             <a href="findjobs.php">Find Jobs</a>
 
                         </li>
-                        <li>
-                            <a href="#">Pages </a>
-                            <ul class="sub-menu">
-                                <li><a href="about.html">About</a></li>
-                                <li><a href="register.html">Register</a></li>
-                                <li><a href="login.html">Login</a></li>
-                                <li><a href="faq.html">FAQ</a></li>
-                                <li><a href="404-error.html">404 Error</a></li>
-                                <li><a href="after-enroll.html">After Enroll</a></li>
-                                <li><a href="courses-admin.html">Instructor Dashboard (Course List)</a></li>
-                                <li><a href="overview.html">Instructor Dashboard (Performance)</a></li>
-                                <li><a href="students.html">Students</a></li>
-                                <li><a href="reviews.html">Reviews</a></li>
-                                <li><a href="engagement.html">Course engagement</a></li>
-                                <li><a href="traffic-conversion.html">Traffic & conversion</a></li>
-                                <li><a href="messages.html">Messages</a></li>
-                            </ul>
-                        </li>
+
                         <li>
                             <a href="#">Blog</a>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-grid.html">Blog</a></li>
-                                        <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                                        <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Blog Details</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-details-left-sidebar.html">Blog Details Left
-                                                Sidebar</a></li>
-                                        <li><a href="blog-details-right-sidebar.html">Blog Details Right
-                                                Sidebar</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
+
                         </li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
@@ -111,8 +76,8 @@
                 <!-- Header Sing In & Up Start -->
                 <div class="header-sign-in-up d-none d-lg-block">
                     <ul>
-                        <li><a class="sign-in" href="login.html">Sign In</a></li>
-                        <li><a class="sign-up" href="register.html">Sign Up</a></li>
+                        <li><a class="sign-in" href="login.html">JobSeeker</a></li>
+                        <li><a class="sign-up" href="register.html">Employer</a></li>
                     </ul>
                 </div>
                 <!-- Header Sing In & Up End -->
