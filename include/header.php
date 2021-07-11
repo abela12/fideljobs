47b7d39fbe85901294de26a69c65d2a9a27dd685
abela12 <abelberhanu6@gmail.com>
2021-07-11 17:23:32 -0600

    update header title and fix some bug

--- include/header.php
+++ include/header.php
@@ -1,12 +1,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Edule - eLearning Website Template</title>
+    <title>ፊደልJobs - Job Finder System</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/icon.png">
 
     <!-- CSS
 	============================================ -->
