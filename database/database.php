ec51f24129371ae472a81fa21faaeb9d0dac4284
abela12 <abelberhanu6@gmail.com>
2021-07-11 17:22:31 -0600

    create database page

---
+++ database/database.php
@@ -0,0 +1,17 @@
+<?php
+$db['host'] = "localhost";
+$db['users'] = "root";
+$db['pass'] = "";
+$db['name'] = "fideljob";
+
+foreach ($db as $key => $value) {
+
+    define(strtoupper($key), $value);
+}
+$conn = mysqli_connect('localhost', 'root', '', 'fideljob');
+$connTest = "";
+if ($conn)
+
+    echo $connTest = "<h6>Database Connected everything is fine 🌝</h6>";
+else
+    echo "not working";
