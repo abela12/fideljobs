e8a6367ee2e4aac121a5bee16baa466132513983
abela12 <abelberhanu6@gmail.com>
2021-07-11 18:27:01 -0600

    create login form

---
+++ include/loginForm.php
@@ -0,0 +1,59 @@
+<div class="section section-padding">
+    <div class="container">
+
+        <!-- Register & Login Wrapper Start -->
+        <div class="register-login-wrapper">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+
+                    <!-- Register & Login Images Start -->
+                    <!-- <div class="register-login-images">
+                        <div class="shape-1">
+                            <img src="assets/images/shape/shape-26.png" alt="Shape">
+                        </div>
+
+
+                        <div class="images">
+                            <img src="assets/images/register-login.png" alt="Register Login">
+                        </div>
+                    </div> -->
+                    <!-- Register & Login Images End -->
+                    <h1>Image Part</h1>
+
+                </div>
+                <div class="col-lg-6">
+
+                    <!-- Register & Login Form Start -->
+                    <div class="register-login-form">
+                        <h3 class="title">Login <span>Now</span></h3>
+
+                        <div class="form-wrapper">
+                            <form action="#">
+                                <!-- Single Form Start -->
+                                <div class="single-form">
+                                    <input type="email" placeholder="Username or Email">
+                                </div>
+                                <!-- Single Form End -->
+                                <!-- Single Form Start -->
+                                <div class="single-form">
+                                    <input type="password" placeholder="Password">
+                                </div>
+                                <!-- Single Form End -->
+                                <!-- Single Form Start -->
+                                <div class="single-form">
+                                    <button class="btn btn-primary btn-hover-dark w-100">Login</button>
+                                    <a class="btn btn-secondary btn-outline w-100" href="#">Login with Google</a>
+                                </div>
+                                <!-- Single Form End -->
+                            </form>
+                        </div>
+                    </div>
+                    <!-- Register & Login Form End -->
+
+                </div>
+            </div>
+        </div>
+        <!-- Register & Login Wrapper End -->
+
+    </div>
+</div>
