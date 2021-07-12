b0d02df2e18fc3aad9fa724b53e05d9c8a2da471
abela12 <abelberhanu6@gmail.com>
2021-07-11 18:27:48 -0600

    create banner page for login

---
+++ include/pageBannerForLogin.php
@@ -0,0 +1,41 @@
+<!-- Page Banner Start -->
+<div class="section page-banner">
+
+    <img class="shape-1 animation-round" src="assets/images/shape/shape-8.png" alt="Shape">
+
+    <img class="shape-2" src="assets/images/shape/shape-23.png" alt="Shape">
+
+    <div class="container">
+        <!-- Page Banner Start -->
+        <div class="page-banner-content">
+            <ul class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li class="active">Login</li>
+            </ul>
+            <h2 class="title">Login <span>Form</span></h2>
+        </div>
+        <!-- Page Banner End -->
+    </div>
+
+    <!-- Shape Icon Box Start -->
+    <div class="shape-icon-box">
+
+        <img class="icon-shape-1 animation-left" src="assets/images/shape/shape-5.png" alt="Shape">
+
+        <div class="box-content">
+            <div class="box-wrapper">
+                <i class="flaticon-badge"></i>
+            </div>
+        </div>
+
+        <img class="icon-shape-2" src="assets/images/shape/shape-6.png" alt="Shape">
+
+    </div>
+    <!-- Shape Icon Box End -->
+
+    <img class="shape-3" src="assets/images/shape/shape-24.png" alt="Shape">
+
+    <!-- <img class="shape-author" src="assets/images/author/author-11.jpg" alt="Shape"> -->
+
+</div>
+<!-- Page Banner End -->
