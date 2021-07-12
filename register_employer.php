937e0639f54d3013366a433b1fe2073dfc2a15f6
abela12 <abelberhanu6@gmail.com>
2021-07-11 18:26:49 -0600

    create registration page for employer

---
+++ register_employer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php include 'include/header.php' ?>
+
+<body>
+
+    <div class="main-wrapper">
+
+        <!-- Header Section Start Navbar Main  -->
+
+        <?php include 'include/navbarMain.php' ?>
+        <!-- Header Section End -->
+
+        <!-- Mobile Menu Start NavBar Mobile Version -->
+        <?php include 'include/navbarMobile.php' ?>
+
+        <!-- Mobile Menu End -->
+
+        <!-- Overlay Start -->
+        <div class="overlay"></div>
+        <!-- Overlay End -->
+        <!-- Page Banner Start -->
+        <?php include 'include/pageBanner.php' ?>
+        <!-- Page Banner End -->
+
+        <!--Job Seeker Register Form -->
+        <?php include 'include/registerJobSeekerForm.php' ?>
+        <!--Job Seeker Register Form -->
+
+
+        <!-- Footer Start  -->
+        <?php //include 'include/footer.php' 
+        ?>
+
+        <!-- Footer End -->
+
+
+
+    </div>
+
+
+
+
+    <?php include 'include/script.php' ?>
+
+</body>
+
+</html>
