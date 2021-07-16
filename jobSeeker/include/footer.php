93ebf0fb59fad3b89d967aa53b02184a2ca0d2c0
abela12 <abelberhanu6@gmail.com>
2021-07-16 13:29:36 -0600

    footer year updated

--- jobSeeker/include/footer.php
+++ jobSeeker/include/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="copyright">
         <p>Copyright © Designed &amp; Developed by <a href="http://blackcoder.com/" target="_blank">🚀BlackCoders🌝</a>
-            2020
+            2021
         </p>
     </div>
 </div>
