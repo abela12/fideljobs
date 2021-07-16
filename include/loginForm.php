7fee11c0b04a2b7b5933a6e3041f150125827ef8
abela12 <abelberhanu6@gmail.com>
2021-07-16 15:20:09 -0600

    fix login name issue

--- include/loginForm.php
+++ include/loginForm.php
@@ -28,20 +28,21 @@
                         <h3 class="title">Login <span>Now</span></h3>
 
                         <div class="form-wrapper">
-                            <form action="#">
+                            <form action="" method="post">
                                 <!-- Single Form Start -->
                                 <div class="single-form">
-                                    <input type="email" placeholder="Username or Email">
+                                    <input name="username" type="text" placeholder="Username or Email">
                                 </div>
                                 <!-- Single Form End -->
                                 <!-- Single Form Start -->
                                 <div class="single-form">
-                                    <input type="password" placeholder="Password">
+                                    <input name="password" type="password" placeholder="Password">
                                 </div>
                                 <!-- Single Form End -->
                                 <!-- Single Form Start -->
                                 <div class="single-form">
-                                    <button class="btn btn-primary btn-hover-dark w-100">Login</button>
+                                    <button type="submit" name="login"
+                                        class="btn btn-primary btn-hover-dark w-100">Login</button>
                                     <a class="btn btn-secondary btn-outline w-100" href="#">Login with Google</a>
                                 </div>
                                 <!-- Single Form End -->
