2daf40b59d83b3705f7790eaf934dbb87038bdcd
abela12 <abelberhanu6@gmail.com>
2021-07-24 23:08:11 -0600

    update content

--- jobSeeker/include/profile.php
+++ jobSeeker/include/profile.php
@@ -14,8 +14,17 @@
             </svg>
         </a>
     </div>
+    <?php
+    $jobseekerID = $_SESSION['id'];
+    $sql = mysqli_query($conn, "SELECT * FROM `jobseeker_social_media` WHERE jobseeker_id ='$jobseekerID'");
+    $num_row = mysqli_num_rows($sql);
+    if ($num_row > 0) {
+
+
+        while ($row = mysqli_fetch_assoc($sql)) {
+    ?>
     <div class="profile-img mb-4">
-        <img src="images/profile/profile.png" width="140" height="140" alt="" />
+        <img src="images/profile/<?php echo $portfolio_image ?>" width="140" height="140" alt="" />
     </div>
     <div class="profile-info-bx mb-4">
         <h4 class="mb-1 fs-22"><?php echo $_SESSION['username'] ?></h4>
@@ -29,6 +38,8 @@
                 class="fa fa-twitter scale5 mr-3"></i>/avel.coder</a>
         <a href="javascript:;" class="btn text-left light btn-dark d-block mb-4 "><i
                 class="fa fa-facebook scale5 mr-3"></i>/avel.coder</a>
+        <a href="javascript:;" class="btn text-left light btn-dark d-block mb-4 "><i
+                class="fa fa-youtube scale5 mr-3"></i>/avel.coder</a>
         <a href="update-profile.php" class="btn btn-outline-dark d-block btn-lg">Edit Profile</a>
     </div>
     <hr />
@@ -36,4 +47,28 @@
         <h5>I am Full Stack Web Developer</h5>
         <a href="javascript:;" class="fa fa-code"></a>
     </div>
+
+    <?php
+        }
+    } else {
+        echo "<h1>Pls Upload your Status</h1>";
+        ?>
+
+    <div class="text-center mb-5">
+        <a href="javascript:;" class="btn text-left light btn-dark d-block mb-3 "><i
+                class="fa fa-linkedin scale5 mr-3"></i>/avel.coder</a>
+        <a href="javascript:;" class="btn text-left light btn-dark d-block mb-3 "><i
+                class="fa fa-twitter scale5 mr-3"></i>/avel.coder</a>
+        <a href="javascript:;" class="btn text-left light btn-dark d-block mb-4 "><i
+                class="fa fa-facebook scale5 mr-3"></i>/avel.coder</a>
+        <a href="javascript:;" class="btn text-left light btn-dark d-block mb-4 "><i
+                class="fa fa-youtube scale5 mr-3"></i>/avel.coder</a>
+
+        <a href="update-profile.php" class="btn btn-outline-dark d-block btn-lg">Edit Profile</a>
+    </div>
+
+    <?php
+    }
+    ?>
+
 </div>
