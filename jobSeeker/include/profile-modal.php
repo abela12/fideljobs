e816aca13eb93acf2a4a232e5aff4e946e3eb920
abela12 <abelberhanu6@gmail.com>
2021-07-27 13:49:21 -0600

    update modal to large

--- jobSeeker/include/profile-modal.php
+++ jobSeeker/include/profile-modal.php
@@ -1,22 +1,32 @@
-<div class="col-xl-4">
+<?php
+if (isset($_POST['upload'])) {
+    echo "It works";
+}
+?>
+<div class="col-xl-6">
     <div class="card">
 
-        <!-- Modal -->
-        <div class="modal fade" id="basicModal">
-            <div class="modal-dialog" role="document">
+        <!-- Modal For Job Seeker Bio -->
+        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Modal title</h5>
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
-                        <form action="" method="post">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+                        <div class="modal-body">
+                            <!-- Enter a single sentence description of your professional skills/experience (e.g. Expert Web Designer with Ajax experience) -->
                             <div class="form-group">
                                 <input type="text" name="" id="" class="form-control input-default" placeholder="Title">
                             </div>
-                            <div class="post-input">
-                                <textarea name="textarea" id="textarea" cols="30" rows="10" class="form-control bg-transparent" placeholder="Please type what you want...."></textarea>
+                            <div class="form-group">
+                                <textarea name="textarea" id="textarea" rows="7" class="form-control bg-transparent" placeholder="Use this space to show clients you have the skills and experience they're looking for.
+
+Describe your strengths and skills
+Highlight projects, accomplishments and education
+Keep it short and make sure it's error-free"></textarea>
 
                             </div>
 
@@ -30,7 +40,6 @@
                                 <input type="file" name="profile_pic" class="form-control-file" id="profile_pic" accept="image/*" onchange="cover_image(event)">
                                 <img class="rounded-circle" id="img" width="150px" />
                             </div>
-                        </form>
 
 
 
@@ -38,11 +47,13 @@
 
 
 
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger light" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger light" data-dismiss="modal">Close</button>
+                            <button type="submit" name="upload" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
