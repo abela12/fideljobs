aa5bc0d9b5f44ec111b637e0a542b1d8579d76ae
abela12 <abelberhanu6@gmail.com>
2021-07-30 18:27:26 -0600

    create ajax form for bio

--- jobSeeker/include/script.php
+++ jobSeeker/include/script.php
@@ -17,28 +17,173 @@
 
 <!-- Dashboard 1 -->
 <script src="assets/js/dashboard/dashboard-1.js"></script>
+<script src="assets/vendor/sweetalert2/dist/sweetalert2.min.js"></script>
+<script src="assets/js/plugins-init/sweetalert.init.js"></script>
+
 
 <!-- Datatable -->
 <script src="assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
 <script src="assets/js/plugins-init/datatables.init.js"></script>
 <script type='text/javascript'>
-    function preview_image(event) {
-        var reader = new FileReader();
-        reader.onload = function() {
-            var output = document.getElementById('output_image');
-            output.src = reader.result;
-        }
-        reader.readAsDataURL(event.target.files[0]);
+function preview_image(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
     }
+    reader.readAsDataURL(event.target.files[0]);
+}
 </script>
 <script type='text/javascript'>
-    function cover_image(event) {
+function cover_image(event) {
 
-        var reader = new FileReader();
-        reader.onload = function() {
-            var output = output = document.getElementById('img');
-            output.src = reader.result;
-        }
-        reader.readAsDataURL(event.target.files[0]);
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = output = document.getElementById('img');
+        output.src = reader.result;
     }
+    reader.readAsDataURL(event.target.files[0]);
+}
 </script>
+<script>
+$(document).ready(function(e) {
+    $("#form").on('submit', (function(e) {
+        e.preventDefault();
+        console.log("hello");
+        $.ajax({
+            url: "action.php",
+            type: "POST",
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function() {
+                //$("#preview").fadeOut();
+                $("#err").fadeOut();
+            },
+            success: function(data) {
+                if (data == 'invalid') {
+                    // invalid file format.
+                    $("#err").html("Invalid File !").fadeIn();
+                } else {
+                    swal("Good job!", "You bio successfully registered!", "success")
+                        .then(
+                            function() {
+                                location.reload();
+                            });
+                    $("#add").modal('hide');
+
+                    // view uploaded file.
+                    $("#preview").html(data).fadeIn();
+                    $("#form")[0].reset();
+
+                }
+            },
+            error: function(e) {
+                $("#err").html(e).fadeIn();
+            }
+        });
+    }));
+    $("#updateForm").on('submit', (function(e) {
+        e.preventDefault();
+        console.log("hello");
+        $.ajax({
+            url: "updateBio.php",
+            type: "POST",
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function() {
+                //$("#preview").fadeOut();
+                $("#err").fadeOut();
+            },
+            success: function(data) {
+                if (data == 'invalid') {
+                    // invalid file format.
+                    $("#err").html("Invalid File !").fadeIn();
+                } else {
+                    swal("Good job!", "You bio successfully registered!", "success")
+                        .then(
+                            function() {
+                                location.reload();
+                            });
+                    $(".update-bio").modal('hide');
+
+                    // view uploaded file.
+                    $("#preview").html(data).fadeIn();
+                    $("#form")[0].reset();
+
+                }
+            },
+            error: function(e) {
+                $("#err").html(e).fadeIn();
+            }
+        });
+    }));
+});
+// $(document).ajaxStop(function() {
+//     window.location.reload();
+// });
+</script>
+<!-- <script type="text/javascript">
+$(document).ready(function() {
+
+
+    $('#btnadd').click(function(e) {
+        e.preventDefault();
+        console.log("button clicked");
+        // let fname = $("#fname").val();
+        // let lname = $("#lname").val();
+        // let image = "hello";
+
+        // mydata = {
+        //     fName: fname,
+        //     lName: lname,
+        //     Image: image
+        // };
+        // console.log(mydata);
+        $.ajax({
+            url: "data.php",
+            method: "POST",
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(data) {
+                console.log(data);
+            }
+        });
+
+    });
+
+    //insert ajax request data
+    $("#submit").click(function(e) {
+        if ($("#formData")[0].checkValidity()) {
+            e.preventDefault();
+
+            $.ajax({
+                url: "action.php",
+                type: "POST",
+                data: $("#formData").serialize() + "&action=insert",
+                success: function(response) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'profile added successfully',
+                    });
+                    $("#add").modal('hide');
+                    $("#formData")[0].reset();
+
+                }
+            });
+        }
+    });
+
+
+
+
+
+
+
+});
+</script> -->
