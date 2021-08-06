f6def1b20e20a69727ca0df29ac5b407e13bdf06
abela12 <abelberhanu6@gmail.com>
2021-08-05 18:04:32 -0600

    create update profile form

---
+++ jobSeeker/include/profile-settings.php
@@ -0,0 +1,60 @@
+<div class="pt-3">
+    <div class="settings-form">
+        <h4 class="text-primary">Account Setting</h4>
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>Email</label>
+                    <input type="email" placeholder="Email" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="country">Country</label>
+                    <input type="text" name="country" id="countrys" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label>City</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>Address</label>
+                    <input type="text" placeholder="Addis abeba" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label>Phone Number</label>
+                    <input type="number" class="form-control">
+                </div>
+            </div>
+            <h4 class="text-primary mb-2">Work Status</h4>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="workAvailability">Work Availability</label>
+                    <input type="text" name="workAvailability" id="workAvailability" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="workExperience">Work Experience</label>
+                    <input type="text" name="workExperience" id="workExperience" class="form-control">
+                </div>
+
+            </div>
+
+
+            <div class="form-group">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="gridCheck" required>
+                    <label class="custom-control-label" for="gridCheck">
+                        Check me out</label>
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Sign
+                in</button>
+        </form>
+    </div>
+</div>
