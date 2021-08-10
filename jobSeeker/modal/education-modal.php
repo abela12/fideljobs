b6e2e1d3c8c5a1acb25781800dbdeb3da038b937
abela12 <abelberhanu6@gmail.com>
2021-08-09 18:18:04 -0600

    rename modal class

--- jobSeeker/modal/education-modal.php
+++ jobSeeker/modal/education-modal.php
@@ -1,5 +1,5 @@
 <!--Add Education  Modal -->
-<div id="education-modal" class="modal fade education-modal" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade education-modal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
