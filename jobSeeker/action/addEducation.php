1b3018f2d1eeb4b0daceb43d4365a6282b216cb6
abela12 <abelberhanu6@gmail.com>
2021-08-14 12:47:33 -0600

    update and integrate to backend

--- jobSeeker/action/addEducation.php
+++ jobSeeker/action/addEducation.php
@@ -23,3 +23,16 @@ if (isset($_POST['addHighestEducation'])) {
         VALUES ('$jobseeker_id','$schoolName','$fieldOfStudy','$CGPA','$description')");
     }
 }
+if (isset($_POST['workExperience'])) {
+    if ($_POST['workTitle'] == '' or $_POST['workCountry'] == '') {
+    } else {
+        $work_country = $_POST['workCountry'];
+        $work_title = $_POST['workTitle'];
+        $start_year = $_POST['startYear'];
+        $end_year = $_POST['endYear'];
+        $current_work = $_POST['currentWork'];
+        $work_description = $_POST['workDescription'];
+        $insertWorkExperience = mysqli_query($conn, "INSERT INTO `jobseeker_work_experience`(`jobseeker_id`, `work_country`, `work_title`, `start_year`, `end_year`, `current_work`, `work_description`) 
+        VALUES ('$jobseeker_id','$work_country','$work_title','$start_year','$end_year','$current_work','$work_description')");
+    }
+}
