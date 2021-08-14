f36434b2655c1b1f5651f46cbce996b90569781f
abela12 <abelberhanu6@gmail.com>
2021-08-13 23:20:44 -0600

    update content

--- jobSeeker/modal/highest-education-modal.php
+++ jobSeeker/modal/highest-education-modal.php
@@ -7,40 +7,62 @@
                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
+            <form action="update-profile.php" method="post">
+                <div class="modal-body">
 
-                <form action="" method="post">
+
                     <div class="form-row">
                         <div class="form-group col-md-8">
-                            <label for="subject">Subject</label>
-                            <input type="text" name="subject" placeholder="St mary university" id="subject"
+                            <label for="schoolName">School Name</label>
+                            <input type="text" name="schoolName" placeholder="St mary university" id="schoolName"
                                 class="form-control">
                         </div>
 
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-8">
-                            <label for="experienceType">Experience Type</label>
-                            <input type="text" name="experienceType" id="experienceType" placeholder=""
+                            <label for="fieldOfStudy">Field Of Study</label>
+                            <input type="text" name="fieldOfStudy" id="fieldOfStudy" placeholder=""
                                 class="form-control">
                         </div>
 
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-8">
-                            <label for="description">Description (Optional)</label>
+                            <label for="educationStatus"> Degree (Optional)</label>
+                            <select name="educationStatus" id="type" class="form-control">
+                                <option value="Associate of Arts and Sciences (AAS)">Associate of Arts and Sciences
+                                    (AAS)</option>
+                                <option value="Associate of Science (AS)">Associate of Science (AS)</option>
+                            </select>
+                        </div>
+
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-8">
+                            <label for="CGPA">CGPA </label>
+                            <input type="number" name="CGPA" placeholder="" id="CGPA" class="form-control">
+                        </div>
+
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-8">
+                            <label for="description">Description</label>
                             <textarea name="description" id="description" class="form-control" cols="10"
                                 rows="4"></textarea>
                         </div>
                     </div>
 
-                </form>
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger btn-sm light" data-dismiss="modal">Close</button>
-                <button type="submit" name="submit" id="submit" class="btn btn-primary btn-sm">Save changes</button>
-            </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger btn-sm light" data-dismiss="modal">Close</button>
+                    <button type="submit" name="addHighestEducation" id="addHighestEducation"
+                        class="btn btn-primary btn-sm">Save
+                        changes</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
