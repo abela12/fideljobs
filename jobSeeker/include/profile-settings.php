46997d284539b9a163312ff1d6b7292a11caa068
abela12 <abelberhanu6@gmail.com>
2021-08-14 18:21:13 -0600

    add dynamic select form

--- jobSeeker/include/profile-settings.php
+++ jobSeeker/include/profile-settings.php
@@ -32,6 +32,30 @@
                     <input type="number" class="form-control">
                 </div>
             </div>
+            <div class="form-group">
+
+                <div class="mb-4">
+                    <h4 class="card-title">Skill</h4>
+                    <p>Add up to 15 skills. Remove skills by deleting tags.</p>
+
+                </div>
+
+                <select id="skill-list" name="states[]" multiple="multiple">
+                    <?php
+                    $select_skills = mysqli_query($conn, "SELECT * FROM `skill_category`");
+                    while ($row = mysqli_fetch_assoc($select_skills)) {
+                        $skill_name = $row['skill_name'];
+                    ?>
+                    <option value="<?php echo $skill_name ?>"><?php echo $skill_name ?></option>
+                    <?php
+                    }
+                    ?>
+
+
+
+                </select>
+
+            </div>
             <h4 class="text-primary mb-2">Work Status</h4>
             <div class="form-row">
                 <div class="form-group col-md-6">
