ac0c871f21e5683f9e239d4d4dd9e6453db9caf5
abela12 <abelberhanu6@gmail.com>
2021-08-27 18:51:07 -0600

    create forget password and functionality

---
+++ forgot-password.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php include 'include/header.php' ?>
+<?php //include 'database/database.php' 
+?>
+<?php //include 'include/session.php' 
+?>
+<?php require_once "controllerUserData.php"; ?>
+
+
+
+
+
+
+<body>
+
+    <div class="main-wrapper">
+
+        <!-- Header Section Start Navbar Main  -->
+
+        <?php include 'include/navbarMain.php' ?>
+        <!-- Header Section End -->
+
+        <!-- Mobile Menu Start NavBar Mobile Version -->
+        <?php include 'include/navbarMobile.php' ?>
+
+        <!-- Mobile Menu End -->
+
+        <!-- Overlay Start -->
+        <div class="overlay"></div>
+        <!-- Overlay End -->
+
+        <!--Login Page Banner Start -->
+        <?php include 'include/pageBannerForLogin.php' ?>
+        <!--Login Page Banner Start -->
+
+        <!--  Login Page Form Start -->
+        <?php include 'include/forget-password-content.php' ?>
+
+        <!--  Login Page Form Start -->
+
+
+        <!-- Footer Start  -->
+        <?php //include 'include/footer.php' 
+        ?>
+
+        <!-- Footer End -->
+
+
+
+    </div>
+
+
+
+
+    <?php include 'include/script.php' ?>
+
+</body>
+
+</html>
