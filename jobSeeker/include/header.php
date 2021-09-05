788e086fb42513b126f82ce9b5297e2d2a151027
abela12 <abelberhanu6@gmail.com>
2021-09-04 23:21:20 -0600

    update jobseeker header file

--- jobSeeker/include/header.php
+++ jobSeeker/include/header.php
@@ -26,69 +26,69 @@
 <?php include '../include/session.php' ?>
 <?php include 'include/function.php' ?>
 <style>
-    .profile ul li {
-        text-transform: uppercase;
-        cursor: pointer;
-    }
+.profile ul li {
+    text-transform: uppercase;
+    cursor: pointer;
+}
 
-    [type="radio"]:checked,
-    [type="radio"]:not(:checked) {
-        position: absolute;
-        left: -9999px;
-    }
+[type="radio"]:checked,
+[type="radio"]:not(:checked) {
+    position: absolute;
+    left: -9999px;
+}
 
-    [type="radio"]:checked+label,
-    [type="radio"]:not(:checked)+label {
-        position: relative;
-        padding-left: 28px;
-        cursor: pointer;
-        line-height: 20px;
-        display: inline-block;
-        color: #333;
-    }
+[type="radio"]:checked+label,
+[type="radio"]:not(:checked)+label {
+    position: relative;
+    padding-left: 28px;
+    cursor: pointer;
+    line-height: 20px;
+    display: inline-block;
+    color: #333;
+}
 
-    [type="radio"]:checked+label:before,
-    [type="radio"]:not(:checked)+label:before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 18px;
-        height: 18px;
-        border: 1px solid #ddd;
-        border-radius: 100%;
-        background: #fff;
-    }
+[type="radio"]:checked+label:before,
+[type="radio"]:not(:checked)+label:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #ddd;
+    border-radius: 100%;
+    background: #fff;
+}
 
-    [type="radio"]:checked+label:after,
-    [type="radio"]:not(:checked)+label:after {
-        content: '';
-        width: 12px;
-        height: 12px;
-        background: #52B141;
-        position: absolute;
-        top: 4px;
-        left: 4px;
-        border-radius: 100%;
-        -webkit-transition: all 0.2s ease;
-        transition: all 0.2s ease;
-    }
+[type="radio"]:checked+label:after,
+[type="radio"]:not(:checked)+label:after {
+    content: '';
+    width: 12px;
+    height: 12px;
+    background: #52B141;
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+}
 
-    [type="radio"]:not(:checked)+label:after {
-        opacity: 0;
-        -webkit-transform: scale(0);
-        transform: scale(0);
-    }
+[type="radio"]:not(:checked)+label:after {
+    opacity: 0;
+    -webkit-transform: scale(0);
+    transform: scale(0);
+}
 
-    [type="radio"]:checked+label:after {
-        opacity: 1;
-        -webkit-transform: scale(1);
-        transform: scale(1);
-    }
+[type="radio"]:checked+label:after {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+}
 
-    .margin-top {
-        margin-top: -100px;
-    }
+.margin-top {
+    margin-top: -100px;
+}
 </style>
 
 <?php
