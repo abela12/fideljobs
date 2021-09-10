e96195b295261fbd783a02798675f3a52da5b4ca
abela12 <abelberhanu6@gmail.com>
2021-09-10 11:58:03 -0600

    add jobseeker session id

--- jobSeeker/jobs-applied.php
+++ jobSeeker/jobs-applied.php
@@ -4,6 +4,10 @@
 <head>
     <?php include 'include/header.php'
     ?>
+    <?php
+
+    $jobseeker_id = $_SESSION['id'];
+    ?>
 
 </head>
 
@@ -72,69 +76,7 @@
                 <!-- row -->
 
 
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4 class="card-title">Applied Jobs List</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="example" class="display min-w850">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-
-                                                <th>Start date</th>
-                                                <th>Salary</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-
-                                            <tr>
-                                                <td>Zemen Bank</td>
-                                                <td>Software Engineer</td>
-                                                <td>Addis Abeba</td>
-
-                                                <td>2021/01/04</td>
-                                                <td>15025</td>
-                                                <td> <span class="badge light badge-warning">Panding</span></td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>ጸሃይ ሪል ስቴት</td>
-                                                <td>Web Developer</td>
-                                                <td>Remote</td>
-
-                                                <td>2021/01/04</td>
-                                                <td>20,000</td>
-                                                <td> <span class="badge light badge-success">Accepted</span></td>
-
-                                            </tr>
-
-
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-
-                                                <th>Start date</th>
-                                                <th>Salary</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <?php include 'job/jobs-applied-content.php' ?>
             </div>
         </div>
         <!--**********************************
