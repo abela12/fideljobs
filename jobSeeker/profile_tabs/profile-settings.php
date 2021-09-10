23d0e6084a01b6b54d7f6a04e529e465f143604d
abela12 <abelberhanu6@gmail.com>
2021-09-10 17:45:59 -0600

    update content

--- jobSeeker/profile_tabs/profile-settings.php
+++ jobSeeker/profile_tabs/profile-settings.php
@@ -1,39 +1,20 @@
-<?php
-$user_id = $_SESSION['id'];
-if (isset($_POST['update'])) {
-    $country = escape($_POST['country']);
-    $city = escape($_POST['city']);
-    $phoneNumber = escape($_POST['phoneNumber']);
-
-    $check_address = mysqli_query($conn, "SELECT * FROM `address` WHERE `jobseeker_id` =  '$user_id'");
-    if ($check_address) {
-        $num_row = mysqli_num_rows($check_address);
-        if ($num_row > 0) {
-            $update_address = mysqli_query($conn, "UPDATE `address` SET `country`='$country',`city`='$city',`phoneNumber`='$phoneNumber' WHERE `jobseeker_id` ='$user_id'");
-        } else {
-            $insert_adress = mysqli_query($conn, "INSERT INTO `address`(`jobseeker_id`, `country`, `city`, `phoneNumber`) VALUES ('$user_id','$country','$city','$phoneNumber')");
-        }
-    }
-}
-
-?>
-
 <div class="pt-3">
     <div class="settings-form">
         <h4 class="text-primary">Account Setting</h4>
-        <form method="post">
+        <form action="" method="post">
             <?php
 
-            $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE `id` = '$user_id'");
+            $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE `id` = '$jobseeker_id'");
             while ($row = mysqli_fetch_assoc($select_user)) {
                 $email = $row['email'];
                 $password = $row['password'];
             }
-            $select_user = mysqli_query($conn, "SELECT * FROM `address` WHERE `jobseeker_id` =  '$user_id'");
+            $select_user = mysqli_query($conn, "SELECT * FROM `address` WHERE `jobseeker_id` =  '$jobseeker_id'");
             while ($row = mysqli_fetch_assoc($select_user)) {
 
                 $city = $row['city'];
                 $country = $row['country'];
+                $phoneNumber = $row['phoneNumber'];
             }
 
             ?>
@@ -73,32 +54,133 @@ if (isset($_POST['update'])) {
             <div class="form-group">
 
                 <div class="mb-4">
-                    <h4 class="card-title">Skill</h4>
+                    <h4 class="card-title">Skill 💪</h4>
                     <p>Add up to 15 skills. Remove skills by deleting tags.</p>
 
                 </div>
 
-                <select id="skill-list" name="states[]" multiple="multiple">
+                <?php
+                $select_skill = "SELECT * FROM `jobseeker_skill` WHERE `jobseeker_id` = '$jobseeker_id'";
+                $skill_num = checkNumRow($select_skill);
+                if ($skill_num) {
+                ?>
+                <select id="skill-list" name="skill[]" multiple="multiple">
                     <?php
-                    $select_skills = mysqli_query($conn, "SELECT * FROM `skill_category`");
-                    while ($row = mysqli_fetch_assoc($select_skills)) {
-                        $skill_name = $row['skill_name'];
-                    ?>
+                        $select_skills = mysqli_query($conn, "SELECT * FROM `jobseeker_skill` WHERE `jobseeker_id` = '$jobseeker_id'");
+                        $skill_name = "";
+                        while ($row = mysqli_fetch_assoc($select_skills)) {
+                            $skill_name = $row['skill_name'];
+                            $skill_name = explode(",", $skill_name);
+                        }
+                        $i = 0;
+                        foreach ($skill_name as $skill) {
+                        ?>
+
+                    <option selected="selected" value="<?php echo $skill ?>"><?php echo $skill ?>
+                    </option>
+                    <?php
+                        }
+                        $select_skills = mysqli_query($conn, "SELECT * FROM `skill_category` ");
+                        while ($row = mysqli_fetch_assoc($select_skills)) {
+                            $skill_name = $row['skill_name'];
+                        ?>
                     <option value="<?php echo $skill_name ?>"><?php echo $skill_name ?></option>
                     <?php
-                    }
+
+                        }
+                        ?>
+
                     ?>
+
 
 
 
                 </select>
 
+                <?php
+
+                } else {
+                ?>
+                <select id="skill-list" name="skill[]" multiple="multiple">
+                    <?php
+                        $select_skills = mysqli_query($conn, "SELECT * FROM `skill_category`");
+                        while ($row = mysqli_fetch_assoc($select_skills)) {
+                            $skill_name = $row['skill_name'];
+                        ?>
+                    <option value="<?php echo $skill_name ?>"><?php echo $skill_name ?></option>
+                    <?php
+                        }
+                        ?>
+
+
+
+                </select>
+                <?php
+                }
+
+                ?>
+
+
             </div>
             <!-- Work Status TODO: Table Name jobseeker_work_status -->
-            <h4 class="text-primary mb-2">Work Status</h4>
+            <h4 class="text-primary mb-2">Work Status ⏲</h4>
             <div class="form-row">
+                <?php
+                $select_work_status = "SELECT * FROM `jobseeker_work_status` WHERE `jobseeker_id` = '$jobseeker_id'";
+                $check_work_num = checkNumRow($select_work_status);
+                if ($check_work_num) {
+                ?>
                 <div class="form-group col-md-6">
-                    <label for="work_availability"> I am currently</label>
+                    <label for="work_availability"> I am currently 👷</label>
+                    <select name="work_availability" id="work_availability" class="form-control">
+                        <?php
+                            $sql = fetchData($select_work_status);
+                            while ($row = mysqli_fetch_assoc($sql)) {
+                                $work_availability = $row['work_availability'];
+                                $job_type = $row['job_type'];
+                            ?>
+                        <option value="<?php echo $work_availability ?>"><?php echo $work_availability ?></option>
+                        <?php
+                            }
+                            ?>
+                        <option value="More then 30 hrs/week">More then 30 hrs/week</option>
+                        <option value="Less then 30 hrs/week">Less then 30 hrs/week</option>
+                        <option value="As needed- open to offers">As needed- open to offers</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="job_type">Job Type</label>
+                    <select name="job_type" id="job_type" class="form-control">
+                        <?php
+                            $sql = fetchData($select_work_status);
+                            while ($row = mysqli_fetch_assoc($sql)) {
+                                $work_availability = $row['work_availability'];
+                                $job_type = $row['job_type'];
+                            ?>
+                        <option value="<?php echo $job_type ?>"><?php echo $job_type ?></option>
+
+                        <?php
+                            }
+                            ?>
+                        <option value="Full Time">Full Time</option>
+                        <option value="Part Time">Part Time
+                        </option>
+
+                        <option value="Permanent">Permanent</option>
+                        <option value="Remote">Remote</option>
+                        <option value="Contractual">Contractual</option>
+                        <option value="freelancer">Freelancer</option>
+                        <option value="Temporary">Temporary</option>
+                        <option value="Internship">Internship</option>
+
+                    </select>
+                </div>
+                <?php
+
+                } else {
+                ?>
+                <div class="form-group col-md-6">
+                    <label for="work_availability"> I am currently 👷</label>
                     <select name="work_availability" id="work_availability" class="form-control">
                         <option value="More then 30 hrs/week">More then 30 hrs/week</option>
                         <option value="Less then 30 hrs/week">Less then 30 hrs/week</option>
@@ -121,6 +203,13 @@ if (isset($_POST['update'])) {
 
                     </select>
                 </div>
+
+                <?php
+
+                }
+
+                ?>
+
 
             </div>
 
