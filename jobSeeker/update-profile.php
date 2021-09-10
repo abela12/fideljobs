21e4bace2a1ab8d323402a668b551f1d83162acf
abela12 <abelberhanu6@gmail.com>
2021-09-10 17:44:30 -0600

    add profile setting page *dynamic*

--- jobSeeker/update-profile.php
+++ jobSeeker/update-profile.php
@@ -4,7 +4,7 @@
 <head>
     <?php include 'include/header.php' ?>
     <?php include 'action/addEducation.php' ?>
-
+    <?php include 'profile_tabs/update_profile_backend.php' ?>
 
 
 
@@ -68,6 +68,24 @@
         ***********************************-->
         <div class="content-body">
             <div class="container-fluid">
+                <div class="col-md-6">
+                    <?php if ($msg == 'yes') : ?>
+
+                    <div class="<?php echo $msgClass ?> solid alert-dismissible fade show col-md-8">
+                        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
+                            <path
+                                d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3">
+                            </path>
+                        </svg>
+                        <strong>Done!</strong> Bio Successfully Updated 🌝
+                        <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i
+                                    class="mdi mdi-close"></i></span>
+                        </button>
+                    </div>
+
+                    <?php endif; ?>
+                </div>
                 <div class="page-titles">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Profile</a></li>
@@ -83,8 +101,10 @@
                 </div>
                 <div class="row">
 
+
                     <div class="col-xl-4">
-                        <div class="card">
+
+                        <div class="card" id="#aboutme">
                             <div class="card-body">
                                 <div class="profile-blog mb-5">
                                     <h5 class="text-primary d-inline">About Me</h5>
@@ -129,7 +149,9 @@
                         </div>
                     </div>
                     <div class="col-xl-8">
+
                         <div class="card">
+
                             <div class="card-body">
                                 <div class="profile-tab">
                                     <div class="custom-tab-1">
@@ -149,97 +171,10 @@
 
                                             </div>
                                             <div id="about-me" class="tab-pane fade">
-                                                <div class="profile-about-me">
-                                                    <div class="pt-4 border-bottom-1 pb-3">
-                                                        <h4 class="text-primary">About Me</h4>
-                                                        <p class="mb-2">My name is Alpha Coder. I'm 22 years old
-                                                            freelance website
-                                                            designer, Originally born in Addis Ababa, Ethiopia, finished
-                                                            high
-                                                            school in my townhome, and now I'm learning Computer
-                                                            Science at St'marry University.
-                                                            My work is entirely based around the website, Back-end
-                                                            development, mobile apps development, and Website Hosting.
-                                                            My active work life consists of maintaining freelance
-                                                            clients
-                                                            and traveling to different cities doing different websites,
-                                                            mobile app developments, and database development.</p>
-
-                                                    </div>
-                                                </div>
-                                                <div class="profile-skills mb-5">
-                                                    <h4 class="text-primary mb-2">Skills</h4>
-                                                    <a href="javascript:void()"
-                                                        class="btn btn-primary light btn-xs mb-1">Java</a>
-                                                    <a href="javascript:void()"
-                                                        class="btn btn-primary light btn-xs mb-1">Wordpress</a>
-                                                    <a href="javascript:void()"
-                                                        class="btn btn-primary light btn-xs mb-1">Photoshop</a>
-
-                                                </div>
-                                                <div class="profile-lang  mb-5">
-                                                    <h4 class="text-primary mb-2">Language</h4>
-                                                    <a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
-                                                            class="flag-icon flag-icon-us"></i> English</a>
-                                                    <a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
-                                                            class="flag-icon flag-icon-fr"></i> Amharic</a>
-                                                    <a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
-                                                            class="flag-icon flag-icon-bd"></i> Oromegna</a>
-                                                </div>
-                                                <div class="profile-personal-info">
-                                                    <h4 class="text-primary mb-4">Personal Information</h4>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Name <span class="pull-right">:</span>
-                                                            </h5>
-                                                        </div>
-                                                        <div class="col-9"><span>Alpha Coder</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Email <span class="pull-right">:</span>
-                                                            </h5>
-                                                        </div>
-                                                        <div class="col-9"><span>alpha@blackcoder.com</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Availability <span
-                                                                    class="pull-right">:</span></h5>
-                                                        </div>
-                                                        <div class="col-9"><span>Full Time (Free Lancer)</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Age <span class="pull-right">:</span>
-                                                            </h5>
-                                                        </div>
-                                                        <div class="col-9"><span>22</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Location <span
-                                                                    class="pull-right">:</span></h5>
-                                                        </div>
-                                                        <div class="col-9"><span>Ethiopia ,Addis Abeba, Bole</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mb-2">
-                                                        <div class="col-3">
-                                                            <h5 class="f-w-500">Year Experience <span
-                                                                    class="pull-right">:</span></h5>
-                                                        </div>
-                                                        <div class="col-9"><span>02 Year Experiences</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                                <?php include 'profile_tabs/about-me-content.php' ?>
                                             </div>
                                             <div id="profile-settings" class="tab-pane fade">
-                                                <?php include 'include/profile-settings.php' ?>
+                                                <?php include 'profile_tabs/profile-settings.php' ?>
                                             </div>
                                         </div>
                                     </div>
