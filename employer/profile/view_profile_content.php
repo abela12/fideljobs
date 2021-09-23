02c74d4d86542a13d7ce5d1b90f8a10920650662
abela12 <abelberhanu6@gmail.com>
2021-09-22 23:53:27 -0600

    create view profile content

---
+++ employer/profile/view_profile_content.php
@@ -0,0 +1,44 @@
+<div class="content-body">
+    <!-- row -->
+    <div class="container-fluid">
+        <div class="form-head d-flex mb-0 mb-lg-4 align-items-start">
+            <div class="mr-auto d-none d-lg-block">
+                <h2 class="text-black font-w600">Employer Dashboard Page</h2>
+                <div>
+                    <a href="javascript:void(0);" class="fs-18 text-primary font-w600">Employer / </a>
+                    <a href="javascript:void(0);" class="fs-18"></a>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            left side bar
+                        </div>
+                        <div>
+                            <span>Add Company Profile</span>
+                            <button type="button" class="btn btn-success mb-2 btn-sm" data-toggle="modal"
+                                data-target=".add-company-profile"><i class="fa fa-plus-circle"></i></button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">right side bar</div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+</div>
