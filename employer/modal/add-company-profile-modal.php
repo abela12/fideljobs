7e215da41cd8aebea3a397a93099c3d88c8a9035
abela12 <abelberhanu6@gmail.com>
2021-09-23 11:02:16 -0600

    fix some bugs and update content

--- employer/modal/add-company-profile-modal.php
+++ employer/modal/add-company-profile-modal.php
@@ -7,28 +7,36 @@
                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
                 </button>
             </div>
-            <form action="update-profile.php" method="post">
+            <form action="profile.php" method="post">
                 <div class="modal-body">
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
+
                             <label for="companyName">Company Name</label>
                             <input type="text" name="company_name" id="company_name" class="form-control">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="company_location">Company Location</label>
-                            <input type="text" name="company_location" id="company_location" class="form-control">
+                            <input type="text" name="company_location" id="company_location" class="form-control"
+                                required>
                         </div>
 
                     </div>
                     <div class="form-row">
-                        <div class="form-group col-md-8">
-                            <label for="company_description">About Company</label>
-                            <textarea class="form-control" name="company_description" id="company_description"
-                                rows="3"></textarea>
-                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="company_short_description">Short Description</label>
 
+                            <textarea name="company_short_description" id="company_short_description" cols="3" rows="3"
+                                class="form-control" required></textarea>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="company_description">About Company</label>
+                            <textarea class="form-control" name="company_description" id="company_description" rows="6"
+                                required></textarea>
+                        </div>
                     </div>
+
 
 
 
@@ -36,7 +44,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger btn-sm light" data-dismiss="modal">Close</button>
-                    <button type="submit" name="workExperience" id="workExperience" class="btn btn-primary btn-sm">Save
+                    <button type="submit" name="addCompanyProfile" id="addCompanyProfile"
+                        class="btn btn-primary btn-sm">Save
                         changes</button>
                 </div>
             </form>
