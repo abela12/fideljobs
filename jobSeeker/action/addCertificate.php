11f9434ada9e8419ed8597591df86434f6e6c9c8
abela12 <abelberhanu6@gmail.com>
2021-09-22 23:50:29 -0600

    added FILTER_SANITIZE_URL

--- jobSeeker/action/addCertificate.php
+++ jobSeeker/action/addCertificate.php
@@ -9,6 +9,11 @@ if ($userData > 0) {
         if (trim($_POST['certificateName'] != '') && trim($_POST['certificateLink'] != '')) {
             $certificateName   = $_POST["certificateName"][$i];
             $certificateLink  = $_POST["certificateLink"][$i];
+            // Remove all illegal characters from a url
+            $certificateLink = filter_var($certificateLink, FILTER_SANITIZE_URL);
+            if (filter_var($url, FILTER_VALIDATE_URL)) {
+            } else {
+            }
             $query  = "INSERT INTO `jobseekercertifications`(`jobseeker_id`, `certificationsName`, `certificationsLink`) 
             VALUES ('$jobseeker_id','$certificateName','$certificateLink')";
             $result = mysqli_query($conn, $query);
