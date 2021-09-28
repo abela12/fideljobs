dec1dc76968db8aa3b47942ab45493f2194dc380
abela12 <abelberhanu6@gmail.com>
2021-09-28 00:31:43 -0600

    update generete report page

--- admin/systemadministrator/generate_report.php
+++ admin/systemadministrator/generate_report.php
@@ -5,52 +5,7 @@
 <?php include '../database/database.php' ?>
 <?php include '../include/session.php' ?>
 <?php require '../include/function.php' ?>
-<?php
-
-$all  = "SELECT * FROM `users`";
-$select_all = mysqli_query($conn, $all);
-$count_all = mysqli_num_rows($select_all);
-$job_seeker = mysqli_query($conn, "SELECT * FROM `users` WHERE `role_id`= 2");
-$employer =  mysqli_query($conn, "SELECT * FROM `users` WHERE `role_id`= 3");
-
-$count_job_seeker = mysqli_num_rows($job_seeker);
-$count_employer = mysqli_num_rows($employer);
-
-$count_job_seeker = (($count_job_seeker / $count_all) * 100);
-$count_employer = (($count_employer / $count_all) * 100);
-
-$dataPoints = array(
-    array("label" => "Job Seeker", "y" => $count_job_seeker),
-    array("label" => "Employer", "y" => $count_employer)
-);
-
-$all_job = mysqli_query($conn, "SELECT * FROM `job`");
-$count_all_job = mysqli_num_rows($all_job);
-$opened_job = mysqli_query($conn, "SELECT * FROM `job` WHERE `job_status` ='Opened'");
-$closed_job = mysqli_query($conn, "SELECT * FROM `job` WHERE `job_status` ='Closed'");
-$delete_job = mysqli_query($conn, "SELECT * FROM `job` WHERE `job_status` ='delete'");
-
-$count_opened_job = mysqli_num_rows($opened_job);
-$count_closed_job = mysqli_num_rows($closed_job);
-$count_delete_job = mysqli_num_rows($delete_job);
-
-$count_opened_job  = (($count_opened_job / $count_all_job) * 100);
-$count_closed_job  = (($count_closed_job / $count_all_job) * 100);
-$count_delete_job  = (($count_delete_job / $count_all_job) * 100);
-$dataPoints1 = array(
-    array("label" => "Opened Job", "y" => $count_opened_job),
-    array("label" => "Closed Job", "y" => $count_employer),
-    array("label" => "Deleted Job", "y" => $count_delete_job)
-);
-
-
-
-
-
-
-
-
-?>
+<?php include 'include/data.php' ?>
 
 
 <body class="nk-body bg-lighter npc-general has-sidebar ">
@@ -79,67 +34,109 @@ $dataPoints1 = array(
                             </div>
                             <div class="card card-preview">
                                 <script>
-                                    window.onload = function() {
+                                window.onload = function() {
 
 
-                                        var chart = new CanvasJS.Chart("chartContainer", {
-                                            theme: "light2",
-                                            animationEnabled: true,
-                                            exportEnabled: true,
-                                            title: {
-                                                text: "Fidel Job Portal Total Users= <?php echo   $count_all ?>  "
+                                    var chart = new CanvasJS.Chart("chartContainer", {
+                                        theme: "light2",
+                                        animationEnabled: true,
+                                        exportEnabled: true,
+                                        title: {
+                                            text: "Fidel Job Portal Total Users= <?php echo   $count_all ?>  "
 
-                                            },
-                                            data: [{
-                                                type: "pie",
-                                                indexLabel: "{y}",
-                                                yValueFormatString: "#,##0.00\"%\"",
-                                                indexLabelPlacement: "inside",
-                                                indexLabelFontColor: "#36454F",
-                                                indexLabelFontSize: 18,
-                                                indexLabelFontWeight: "bolder",
-                                                showInLegend: true,
-                                                legendText: "{label}",
-                                                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-                                            }]
-                                        });
-                                        chart.render();
+                                        },
+                                        data: [{
+                                            type: "pie",
+                                            indexLabel: "{y}",
+                                            yValueFormatString: "#,##0.00\"%\"",
+                                            indexLabelPlacement: "inside",
+                                            indexLabelFontColor: "#36454F",
+                                            indexLabelFontSize: 18,
+                                            indexLabelFontWeight: "bolder",
+                                            showInLegend: true,
+                                            legendText: "{label}",
+                                            dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                                        }]
+                                    });
+                                    chart.render();
 
-                                        var chart1 = new CanvasJS.Chart("chartContainer1", {
-                                            theme: "light2",
-                                            animationEnabled: true,
-                                            exportEnabled: true,
-                                            title: {
-                                                text: "Fidel Job Portal Total Jobs= <?php echo   $count_all_job ?>  "
+                                    var chart1 = new CanvasJS.Chart("chartContainer1", {
+                                        animationEnabled: true,
+                                        theme: "light2",
+                                        title: {
+                                            text: "Job Status- Total Jobs <?php echo $count_all_job ?>"
+                                        },
+                                        axisY: {
+                                            suffix: "%",
+                                            scaleBreaks: {
+                                                autoCalculate: true
+                                            }
+                                        },
+                                        data: [{
+                                            type: "column",
+                                            yValueFormatString: "#,##0\"%\"",
+                                            indexLabel: "{y}",
+                                            indexLabelPlacement: "inside",
+                                            indexLabelFontColor: "white",
 
-                                            },
-                                            data: [{
-                                                type: "pie",
-                                                indexLabel: "{y}",
-                                                yValueFormatString: "#,##0.00\"%\"",
-                                                indexLabelPlacement: "inside",
-                                                indexLabelFontColor: "#36454F",
-                                                indexLabelFontSize: 18,
-                                                indexLabelFontWeight: "bolder",
-                                                showInLegend: true,
-                                                legendText: "{label}",
-                                                dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
-                                            }]
-                                        });
-                                        chart1.render();
+                                            showInLegend: true,
+                                            dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+                                        }]
+                                    });
+                                    chart1.render();
 
-                                    }
+                                    var chart2 = new CanvasJS.Chart("chartContainer2", {
+                                        animationEnabled: true,
+                                        theme: "light2",
+                                        title: {
+                                            text: "Job Status- Total Jobs <?php echo $count_all_job ?>"
+                                        },
+                                        axisY: {
+                                            suffix: "%",
+                                            scaleBreaks: {
+                                                autoCalculate: true
+                                            }
+                                        },
+                                        data: [{
+                                            type: "column",
+                                            yValueFormatString: "#,##0\"%\"",
+                                            indexLabel: "{y}",
+                                            indexLabelPlacement: "inside",
+                                            indexLabelFontColor: "white",
+
+
+                                            dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+                                        }]
+                                    });
+                                    chart2.render();
+
+                                }
                                 </script>
-                                <div id="chartContainer" style="height: 370px; width: 100%;">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div id="chartContainer" style="height: 370px; width: 100%;">
 
 
 
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div id="chartContainer1" style="height: 370px; width: 100%;">
+
+
+
+                                        </div><!-- .card-preview -->
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div id="chartContainer2" style="height: 370px; width: 100%;">
+
+
+
+                                        </div><!-- .card-preview -->
+                                    </div>
                                 </div>
-                                <div id="chartContainer1" style="height: 370px; width: 100%;">
 
 
-
-                                </div><!-- .card-preview -->
                             </div>
                         </div>
                     </div>
