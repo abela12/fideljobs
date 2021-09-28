cb3c5cfa50a9e872e27bee435f1bcc0ee43138ba
abela12 <abelberhanu6@gmail.com>
2021-09-28 00:32:45 -0600

    add some Statistics on admin page

--- admin/systemadministrator/index.php
+++ admin/systemadministrator/index.php
@@ -5,9 +5,19 @@
 <?php include '../database/database.php' ?>
 <?php include '../include/session.php' ?>
 <?php require '../include/function.php' ?>
+<?php require 'include/data.php' ?>
+
 <?php
 //login confirmation
 confirm_logged_in();
+$all  = "SELECT * FROM `users`";
+$select_all = mysqli_query($conn, $all);
+$count_all = mysqli_num_rows($select_all);
+$job_seeker = mysqli_query($conn, "SELECT * FROM `users` WHERE `role_id`= 2");
+$employer =  mysqli_query($conn, "SELECT * FROM `users` WHERE `role_id`= 3");
+
+$count_job_seeker = mysqli_num_rows($job_seeker);
+$count_employer = mysqli_num_rows($employer);
 
 ?>
 
@@ -34,6 +44,109 @@ confirm_logged_in();
                                 <div class="nk-block">
 
                                 </div><!-- .nk-block -->
+                                <div class="row g-4">
+                                    <div class="col-sm-6 col-xxl-12">
+                                        <div class="nk-order-ovwg-data buy">
+                                            <div class="amount"><?php echo $count_all ?> <small
+                                                    class="currenct currency-usd">User</small></div>
+                                            <div class="info">Job Seeker <strong><?php echo $count_job_seeker ?> <span
+                                                        class="currenct currency-usd">🌝</span></strong></div>
+                                            <div class="info">Employers<strong><?php echo $count_employer ?> <span
+                                                        class="currenct currency-usd">👲</span></strong></div>
+                                            <div class="title"><em class="icon ni ni-arrow-down-left"></em><a
+                                                    href="systemadministrator/manage_employer.php">Manage Employer</a>
+
+                                            </div>
+                                            <div class="title"><em class="icon ni ni-arrow-down-left"></em><a
+                                                    href="systemadministrator/manage_jobseeker.php"> Manage Job
+                                                    seeker</a>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card card-preview">
+                                    <script>
+                                    window.onload = function() {
+
+
+
+
+                                        var chart1 = new CanvasJS.Chart("chartContainer1", {
+                                            animationEnabled: true,
+                                            theme: "light2",
+                                            title: {
+                                                text: "Job Status- Total Jobs <?php echo $count_all_job ?>"
+                                            },
+                                            axisY: {
+                                                suffix: "%",
+                                                scaleBreaks: {
+                                                    autoCalculate: true
+                                                }
+                                            },
+                                            data: [{
+                                                type: "column",
+                                                yValueFormatString: "#,##0\"%\"",
+                                                indexLabel: "{y}",
+                                                indexLabelPlacement: "inside",
+                                                indexLabelFontColor: "white",
+
+                                                showInLegend: true,
+                                                dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+                                            }]
+                                        });
+                                        chart1.render();
+
+                                        var chart2 = new CanvasJS.Chart("chartContainer2", {
+                                            animationEnabled: true,
+                                            theme: "light2",
+                                            title: {
+                                                text: "Job Status- Total Jobs <?php echo $count_all_job ?>"
+                                            },
+                                            axisY: {
+                                                suffix: "%",
+                                                scaleBreaks: {
+                                                    autoCalculate: true
+                                                }
+                                            },
+                                            data: [{
+                                                type: "column",
+                                                yValueFormatString: "#,##0\"%\"",
+                                                indexLabel: "{y}",
+                                                indexLabelPlacement: "inside",
+                                                indexLabelFontColor: "white",
+
+
+                                                dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+                                            }]
+                                        });
+                                        chart2.render();
+
+                                    }
+                                    </script>
+                                    <div class="row">
+
+
+
+                                        <div class="col-md-6">
+                                            <div id="chartContainer1" style="height: 370px; width: 100%;">
+
+
+
+                                            </div><!-- .card-preview -->
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div id="chartContainer2" style="height: 370px; width: 100%;">
+
+
+
+                                            </div><!-- .card-preview -->
+                                        </div>
+                                    </div>
+
+
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -49,6 +162,8 @@ confirm_logged_in();
     </div>
     <!-- app-root @e -->
     <!-- JavaScript -->
+    <script src="./assets/js/canvasjs.min.js"></script>
+
     <script src="./assets/js/bundle.js?ver=2.4.0"></script>
     <script src="./assets/js/scripts.js?ver=2.4.0"></script>
     <script src="./assets/js/charts/gd-default.js?ver=2.4.0"></script>
